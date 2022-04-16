e70687b01b726a5d6c1abb7afd6af407a52cdb89
Artem Paliutin <pal.artem@icloud.com>
2022-04-16 13:17:20 +0300

    Initial Commit

---
+++ AuthenticatorDemo/AuthenticatorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticatorDemoApp.swift
+//  AuthenticatorDemo
+//
+//  Created by Artem Paliutin on 16/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticatorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
